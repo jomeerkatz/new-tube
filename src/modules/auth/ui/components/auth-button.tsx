cd18d8891b59132538ff673bc8da17e46c21baa3
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-10-06 18:10:40 +0200

    add clerk auth

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,11 +1,22 @@
+"use client";
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { UserCircleIcon } from "lucide-react";
 const AuthButton = () => {
   return (
-    <Button>
-      <UserCircleIcon />
-      Sign In
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton></UserButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>
+            <UserCircleIcon />
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
 
